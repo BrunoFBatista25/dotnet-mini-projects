7fbd803d78d48a710450e28efa2f18aae81e0d5b
lucia <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>
2026-05-22 23:27:39 -0300

    Atualização do Repositório de Projeto.

---
+++ src/Calculadora/calculadora.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class calculadora {
+
+    int somar(int i; int j) { 
+        return i+j;
+    }
+
+    int subtrair(int i; int j) { 
+        return i-j;
+    }
+    float dividir(int i; int j) {
+    return i/j; 
+    }
+    float multiplicar(int i; int j) {
+    return i*j;
+    }
+
+}
